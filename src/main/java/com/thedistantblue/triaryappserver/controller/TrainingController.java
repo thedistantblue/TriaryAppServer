82483ad53e467a291aeaced85c9708a25b271ab5
DistantBlue <exotic.xD@gmail.com>
2019-08-25 22:09:15 +0300

    Small changes

--- src/main/java/com/thedistantblue/triaryappserver/controller/TrainingController.java
+++ src/main/java/com/thedistantblue/triaryappserver/controller/TrainingController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
 @RestController
 @RequestMapping(path = "/training", produces = "application/json")
 public class TrainingController {
